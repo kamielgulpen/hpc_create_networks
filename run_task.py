44eae4f0cdfd6eb580ece86cfa1325d376e6d222
kamielgulpen <k.gulpen@hotmail.nl>
2026-04-29 17:04:34 +0200

    added transitivity as parameter sweep

--- run_task.py
+++ run_task.py
@@ -33,7 +33,7 @@ RECIPROCITY_P    = 1
 
 PREF_ATTACHMENT_VALUES    = np.linspace(0, 0.9999, 2)
 N_COMMUNITIES_VALUES      = np.linspace(5000, 20000, 10).astype(int)
-TRANSITIVITY_VALUES       = np.array([0])
+TRANSITIVITY_VALUES       = np.linspace(0,1,3)
 BRIDGE_PROBABILITY_VALUES = np.array([0])
 
 
