7bb92e46066a2a56ff79de452c8d605184a2438a
kamielgulpen <k.gulpen@hotmail.nl>
2026-07-08 12:42:59 +0200

    only 1 threshold

--- ETL/4_seeding_experiment/seeding_experiments.py
+++ ETL/4_seeding_experiment/seeding_experiments.py
@@ -360,9 +360,9 @@ class SimulationConfig:
     max_steps:                 int   = 10000
     threshold_type:            str   = 'fractional'
     initial_infected_fraction: float = 0.01
-    min_threshold:             float = 0.2
+    min_threshold:             float = 0.3
     max_threshold:             float = 0.3
-    n_thresholds:              int   = 2
+    n_thresholds:              int   = 1
     base_seed:                 int   = 0
     verbose:                   int   = 0
     seeding:                   str   = 'neighbor_k'
