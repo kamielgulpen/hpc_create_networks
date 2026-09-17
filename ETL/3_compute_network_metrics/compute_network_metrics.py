77ce0c48dce8a0d039e927b0ea4a9408c98a7d78
kamielgulpen <k.gulpen@hotmail.nl>
2026-09-17 09:37:28 +0200

    added datalake

--- ETL/3_compute_network_metrics/compute_network_metrics.py
+++ ETL/3_compute_network_metrics/compute_network_metrics.py
@@ -44,7 +44,7 @@ import igraph as ig
 import numpy as np
 import pandas as pd
 from scipy import stats
-
+import sys
 LAKE_DIR = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(1, str(LAKE_DIR))
 import data_lake
